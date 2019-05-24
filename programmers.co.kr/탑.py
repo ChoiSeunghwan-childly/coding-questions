150c977e78c5dafae949e1cd168a9cb93f66ccb7
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2019-05-24 16:28:14 +0900

    coding test

---
+++ programmers.co.kr/탑.py
@@ -0,0 +1,12 @@
+def solution(heights):
+    answer = []
+    for i in range(len(heights)):
+        answer.append(0)
+    
+    for i in range(1 , len(heights)):
+        for j in range( i -1 , -1 , -1 ):
+            if heights[i] < heights[j]:
+                answer[i] = j + 1
+                break
+    
+    return answer
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-testst
+
+for i in range( 1, 5):
+    for j in range( i -1 , -1 , -1):
+        print(i , j )
