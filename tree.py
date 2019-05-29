a2bfd4f207f89c64c2c0c92633e253d9aed20d63
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2019-05-29 13:23:13 +0900

    Tree

--- tree.py
+++ tree.py
@@ -1,3 +1,5 @@
+#  http://ejklike.github.io/2018/01/09/traversing-a-binary-tree-1.html#fn:1
+
 class Node:
     def __init__(self, data=0, left = None , right = None):
         self.data = data
@@ -21,4 +23,62 @@ class BinaryTree:
             else:
                 node.right = self.insert_value(node.right, data)
         return node
-        
+    
+    def delete(self, key):
+        self.root, deleted = self.delete_value(self.root, key)
+        return deleted
+
+    def delete_value(self, node, key):
+        if node is None:
+            return node, False
+
+        deleted = False
+        if key == node.data:
+            deleted = True
+            if node.left and node.right:
+                #replace the node to the leftmost of node.right
+                parent, child = node, node.right
+                
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            elif node.left or node.right:
+                node = node.left or node.right
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self.delete_value(node.left, key)
+        else:
+            node.right, deleted = self.delete_value(node.right, key)
+        return node, deleted
+
+    def print_pre_order(self, root):
+        if root:
+            print(root.data)
+            self.print_pre_order(root.left)
+            self.print_pre_order(root.right)
+
+    def print_in_order(self, root):
+        if root:
+            self.print_in_order(root.left)
+            print(root.data)
+            self.print_in_order(root.right)
+
+    def print_post_order(self, root):
+        if root:
+            self.print_post_order(root.left)
+            self.print_post_order(root.right)
+            print(root.data)
+
+arr = [21, 28, 14, 32, 25, 18, 11, 30, 19, 15]
+
+bst = BinaryTree()
+for item in arr:
+    bst.insert(item)
+
+bst.print_pre_order(bst.root)
