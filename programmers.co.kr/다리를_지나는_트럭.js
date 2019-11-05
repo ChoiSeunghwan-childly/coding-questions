30ec59197998f8ec077a43afe788e1c714bb8af5
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2019-11-06 08:58:03 +0900

    다리를 지나는 트럭 풀음.

--- programmers.co.kr/다리를_지나는_트럭.js
+++ programmers.co.kr/다리를_지나는_트럭.js
@@ -6,26 +6,31 @@ function solution(bridge_length, weight, truck_weights) {
     while(truck_weights.length || bridge_q.length){
         sec += 1
         
+         //process
+        for(let i = 0; i < bridge_q.length; i++)
+            bridge_q[i].distance -= 1;
+               
         //dequeue
         if(bridge_q[0])
-            if(bridge_q[0].distance === 0){
-                capacity += bridge_q.weight;
+            if(bridge_q[0].distance <= 0){
+                capacity += bridge_q[0].weight;
                 bridge_q.shift();
                 
                 if(capacity > weight)
                     return "err";
             }
+        
         //enqueue
         if(truck_weights[0])
             if(truck_weights[0] <= capacity){
                 capacity -= truck_weights[0];
                 bridge_q.push({distance: bridge_length, 
-                weight: truck_weights.shift()});            
+                weight: truck_weights.shift()});     
+                
+                if(capacity < 0 )
+                    return "capaErr";
             }
-        
-        //process
-        for(let i = 0; i < bridge_q.length; i++)
-            bridge_q[i].distance -= 1;
+       
     }
     return sec;
 }
