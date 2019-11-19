864d09662ab7cfaa95d063eca57b5c81cf77d0bc
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2019-11-19 23:17:12 +0900

    Update 프린터.js
    프린터 다 품

--- programmers.co.kr/프린터.js
+++ programmers.co.kr/프린터.js
@@ -1,26 +1,23 @@
 function solution(p, l) {
     let ans = 0;
-    
+    let max = Math.max.apply(null, p)
     while(p.length){
         let i = p.shift();
         
-        if (i === Math.max.apply(null, p)){
-            ans += 1;
-            
+        if (i === max){
+            ans += 1;            
             if (l === 0)
                 break;
             else 
-                l -= 1;
-            
+                l -= 1;    
+            max = Math.max.apply(null, p);
         }else{
-            p.push(i);
-            
+            p.push(i);            
             if(l === 0)
                 l = p.length -1;
             else
                 l -= 1;            
         }
-    }
-    
+    }    
     return ans;
 }
